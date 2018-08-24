fbe38ed6de13a47b976cbb6bd82ccb25ec1b40bc
luisjavier.prieto <luisjavier.prieto@juntaex.es>
2018-08-24 14:12:11 +0200

    internet en el metro seguimos

--- src/com/prieto/InternetEnElMetro.java
+++ src/com/prieto/InternetEnElMetro.java
@@ -13,9 +13,29 @@ public class InternetEnElMetro {
 		int numCasos = Integer.valueOf(sc.nextLine());
 		
 		for (int prueba = 0; prueba < numCasos; prueba++ ){
+			System.out.println("Longitud del tunel y Numero de Antenas");
 			entrada = sc.nextLine().split(" ");
 			System.out.println("Longitud Tunel:" + entrada[0]);
 			System.out.println("Numero de Antenas:" + entrada[1]);
+			
+			String [] antenasDistanciaCobertura  = new String[Integer.valueOf(entrada[1])];
+			System.out.println("Lonitud y Distancias");
+			antenasDistanciaCobertura = sc.nextLine().split(" ");
+			boolean coberMin = false;
+			int coberMax = 0;
+			for (int i = 0; i < antenasDistanciaCobertura.length; i=i+2) {
+				//Distancia mayor que la cobertura --> mirar otra antena
+				if (!coberMin && Integer.valueOf(antenasDistanciaCobertura[0])-Integer.valueOf(antenasDistanciaCobertura[1])<=0) {
+					coberMin=true;
+					coberMax=Integer.valueOf(antenasDistanciaCobertura[0])+Integer.valueOf(antenasDistanciaCobertura[1]);
+				}else {
+					continue;	
+				}
+					
+				
+				System.out.println(antenasDistanciaCobertura[i]);
+			}
+		
 		}
 		
 		sc.close();
