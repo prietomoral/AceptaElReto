7bc34f77cf71d4817ff60b18cbd34b11cbaa4150
Luis <Luis@DESKTOP-6P5KRML.home>
2018-08-26 13:05:27 +0200

    loteria atletica resuelta

---
+++ src/com/prieto/LoteriaAtletico.java
@@ -0,0 +1,31 @@
+package com.prieto;
+
+public class LoteriaAtletico {
+	static java.util.Scanner sc;
+
+	public static void casosdeprueba() {
+		int numDecimos = sc.nextInt();
+		String[] decimos = new String [numDecimos];
+		int cont = 0;
+		if (numDecimos > 0 && numDecimos <= 10000) {
+			sc.nextLine();
+			decimos = sc.nextLine().split(" ");
+			for (int i = 0; i < decimos.length; i++) {
+				if (Integer.valueOf(decimos[i]) % 2 == 0 && Integer.valueOf(decimos[i]) >= 0 && Integer.valueOf(decimos[i]) <= 99999)
+					cont++;
+
+			}
+		}
+		System.out.println(cont);
+	}
+
+	public static void main(String[] args) {
+		sc = new java.util.Scanner(System.in);
+		int numcasos = sc.nextInt();
+		for (int i = 0; i < numcasos; i++) {
+			casosdeprueba();
+		}
+
+	}
+
+}
---
+++ src/com/prieto/PlantillaReto.java
@@ -0,0 +1,24 @@
+package com.prieto;
+
+public class PlantillaReto {
+	static java.util.Scanner in;
+	
+	
+	public static void casosdeprueba() {
+		Integer n = in.nextInt();
+	    System.out.println(n);
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		in = new java.util.Scanner(System.in);
+		int numcasos = in.nextInt();
+		for (int i = 0; i < numcasos; i++) {
+			casosdeprueba();
+		}
+
+	}
+
+}
