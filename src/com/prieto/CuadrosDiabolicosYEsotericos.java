e358622366053d59403773817d631a1f4fe2e7e3
Luis <Luis@DESKTOP-6P5KRML.home>
2018-09-01 16:00:04 +0200

    Esoterico hecho

--- src/com/prieto/CuadrosDiabolicosYEsotericos.java
+++ src/com/prieto/CuadrosDiabolicosYEsotericos.java
@@ -5,13 +5,68 @@ public class CuadrosDiabolicosYEsotericos {
 
     static int matriz[][] = null;
 
+    static int cm = 0;
+
+    static int cm2 = 0;
+
     private static void llenarMatriz(int n) {
         matriz = new int[n][n];
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz.length; j++) {
-                matriz[i][j] = sc.nextInt();// EL OBJETO DE TIPO SCANNER LEE
+                matriz[i][j] = sc.nextInt();
             }
+
         }
+    }
+
+    private static void imprimirMatriz() {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void cmycm2(int n) {
+        for (int i = 0; i < n; i++) {
+            cm = cm + matriz[0][i];
+        }
+        cm2 = (4 * cm) / n;
+
+        System.out.println(cm);
+        System.out.println(cm2);
+    }
+
+    private static boolean diabolico() {
+        int totalFila = 0;
+        int totalCol = 0;
+        int diagonal = 0;
+        int diagonalInversa = 0;
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                if (i == j) {
+                    diagonal = diagonal + matriz[i][j];
+                }
+                if (i+j == matriz.length-1) {
+                    diagonalInversa = diagonalInversa + matriz[i][j];
+                }
+                totalFila = totalFila + matriz[i][j];
+                totalCol = totalCol +  matriz[j][i];
+            }
+            if (totalFila != cm || totalCol != cm) {
+                return false;
+            }
+            totalFila = 0;
+            totalCol=0;
+        }
+        
+        
+        return (diagonal == cm && diagonalInversa==cm);
+    }
+
+    private static boolean esoterico() {
+        return false;
     }
 
     public static void main(String[] args) {
@@ -20,6 +75,17 @@ public class CuadrosDiabolicosYEsotericos {
 
         while (n != 0) {
             llenarMatriz(n);
+            imprimirMatriz();
+            cmycm2(n);
+            if (diabolico()) {
+                if (esoterico())
+                    System.out.println("ESOTERICO");
+                else
+                    System.out.println("DIABOLICO");
+            } else {
+                System.out.println("NO");
+            }
+           
 
             n = sc.nextInt();
         }
