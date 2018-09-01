397d11c2180fd86bb035caa4b7c0634420fb8a42
Luis <Luis@DESKTOP-6P5KRML.home>
2018-09-01 13:30:53 +0200

    formateado

--- src/com/prieto/CuadrosDiabolicosYEsotericos.java
+++ src/com/prieto/CuadrosDiabolicosYEsotericos.java
@@ -1,30 +1,31 @@
 package com.prieto;
 
 public class CuadrosDiabolicosYEsotericos {
-	static java.util.Scanner sc;
-	static int matriz[][] = null;
+    static java.util.Scanner sc;
 
-	private static void llenarMatriz(int n) {
-		matriz = new int[n][n];
-		for (int i = 0; i < matriz.length; i++) {
-			for (int j = 0; j < matriz.length; j++) {
-				matriz[i][j] = sc.nextInt();// EL OBJETO DE TIPO SCANNER LEE
-			}
-		}
-	}
+    static int matriz[][] = null;
 
-	public static void main(String[] args) {
-		sc = new java.util.Scanner(System.in);
-		int n = sc.nextInt();
+    private static void llenarMatriz(int n) {
+        matriz = new int[n][n];
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                matriz[i][j] = sc.nextInt();// EL OBJETO DE TIPO SCANNER LEE
+            }
+        }
+    }
 
-		while (n != 0) {
-			llenarMatriz(n);
+    public static void main(String[] args) {
+        sc = new java.util.Scanner(System.in);
+        int n = sc.nextInt();
 
-			n = sc.nextInt();
-		}
+        while (n != 0) {
+            llenarMatriz(n);
 
-		sc.close();
-		System.exit(0);
-	}
+            n = sc.nextInt();
+        }
+
+        sc.close();
+        System.exit(0);
+    }
 
 }
