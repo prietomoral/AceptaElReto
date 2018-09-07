879b6895110f7169c74bd4973d1444c216ef5beb
Luis <prietomoral@gmail.com>
2018-09-07 19:19:25 +0200

    barJavier tampoco

--- src/com/prieto/BarJavier.java
+++ src/com/prieto/BarJavier.java
@@ -41,6 +41,9 @@ public class BarJavier {
         String[] entrada = null;
         while (!input.equals("")) {
             entrada = input.split(" ");
+            String categoria = entrada[0];
+            float valor = Float.parseFloat(entrada[1]);
+
             float valorTotal = 0;
             int numVentas = 0;
             float valorTotalComidas = 0;
@@ -50,10 +53,9 @@ public class BarJavier {
             ventas.put("M", (float) 0);
             ventas.put("I", (float) 0);
             ventas.put("C", (float) 0);
-            String categoria = entrada[0];
-            float valor = Float.parseFloat(entrada[1]);
 
-            while (!(categoria.equals("N") && valor == 0)) {
+            while (categoria.equals("D") || categoria.equals("A") || categoria.equals("M") || categoria.equals("I")
+                    || categoria.equals("C")) {
                 ventas.put(categoria, (float) (ventas.get(categoria) + valor));
                 valorTotal = valorTotal + valor;
                 numVentas++;
@@ -68,63 +70,64 @@ public class BarJavier {
             }
             // Tenemos el diccionario Ventas relleno, ahora hay que determinar el maximo ,minimo, empate maximo o empate minimo
 
-            double maximo = 0.0;
-            double minimo = 0.0;
-            String maxCategoria = "";
-            String minCategoria = "";
-            boolean empateminimo = false;
-            boolean empatemaximo = false;
-            Iterator<String> it = ventas.keySet().iterator();
-            for (int i = 0; i < 5; i++) {
-                String key = (String) it.next();
-                if (i == 0) {
-                    maximo = ventas.get(key);
-                    minimo = ventas.get(key);
-                    minCategoria = key;
-                    maxCategoria = key;
-                } else {
-                    if (ventas.get(key) < minimo) {
+            if (categoria.equals("N") && valor == 0) {
+
+                double maximo = 0.0;
+                double minimo = 0.0;
+                String maxCategoria = "";
+                String minCategoria = "";
+                boolean empateminimo = false;
+                boolean empatemaximo = false;
+                Iterator<String> it = ventas.keySet().iterator();
+                for (int i = 0; i < 5; i++) {
+                    String key = (String) it.next();
+                    if (i == 0) {
+                        maximo = ventas.get(key);
                         minimo = ventas.get(key);
                         minCategoria = key;
-                        empateminimo = false;
-                    } else if (ventas.get(key) > maximo) {
-                        maximo = ventas.get(key);
                         maxCategoria = key;
-                        empatemaximo = false;
-                    } else if (ventas.get(key) == minimo)
-                        empateminimo = true;
-                    else if (ventas.get(key) == maximo)
-                        empatemaximo = true;
+                    } else {
+                        if (ventas.get(key) < minimo) {
+                            minimo = ventas.get(key);
+                            minCategoria = key;
+                            empateminimo = false;
+                        } else if (ventas.get(key) > maximo) {
+                            maximo = ventas.get(key);
+                            maxCategoria = key;
+                            empatemaximo = false;
+                        } else if (ventas.get(key) == minimo)
+                            empateminimo = true;
+                        else if (ventas.get(key) == maximo)
+                            empatemaximo = true;
+                    }
                 }
-            }
 
-            if (minimo == maximo) {
-                empateminimo = true;
-                empatemaximo = true;
-            }
+                if (minimo == maximo) {
+                    empateminimo = true;
+                    empatemaximo = true;
+                }
 
-            if (empatemaximo) {
-                System.out.print("EMPATE#");
-            } else {
-                System.out.print(convertirCodigoACategoria(maxCategoria) + "#");
-            }
-            if (empateminimo) {
-                System.out.print("EMPATE#");
-            } else {
-                System.out.print(convertirCodigoACategoria(minCategoria) + "#");
-            }
-            
-            String mayorMediaComidas = "NO";
-            float resultadoTotalComidas = 0;
-            if (numVentasComidas > 0)
-                resultadoTotalComidas = valorTotalComidas / numVentasComidas;
-            float resultadoTotal = valorTotal / numVentas;
-            if (resultadoTotalComidas > resultadoTotal)
-                mayorMediaComidas = "SI";
-            
-            System.out.println(mayorMediaComidas);
-            
+                if (empatemaximo) {
+                    System.out.print("EMPATE#");
+                } else {
+                    System.out.print(convertirCodigoACategoria(maxCategoria) + "#");
+                }
+                if (empateminimo) {
+                    System.out.print("EMPATE#");
+                } else {
+                    System.out.print(convertirCodigoACategoria(minCategoria) + "#");
+                }
 
+                String mayorMediaComidas = "NO";
+                float resultadoTotalComidas = 0;
+                if (numVentasComidas > 0)
+                    resultadoTotalComidas = valorTotalComidas / numVentasComidas;
+                float resultadoTotal = valorTotal / numVentas;
+                if (resultadoTotalComidas > resultadoTotal)
+                    mayorMediaComidas = "SI";
+
+                System.out.println(mayorMediaComidas);
+            }
             input = sc.nextLine();
 
         }
