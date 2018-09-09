1eb7bd1fa1117ec2c75ad09bca48ebdcec40f0b5
Luis <prietomoral@gmail.com>
2018-09-09 18:05:40 +0200

    Refactorizado y controlando errores en Bar Javier

--- src/com/prieto/BarJavier.java
+++ src/com/prieto/BarJavier.java
@@ -7,6 +7,28 @@ import java.util.Map;
 public class BarJavier {
     static java.util.Scanner sc;
 
+    static Map<String, Float> ventas = new HashMap<String, Float>();
+
+    static float valorTotal = 0;
+
+    static int numVentas = 0;
+
+    static float valorTotalComidas = 0;
+
+    static int numVentasComidas = 0;
+
+    static String maxCategoria = "";
+
+    static String minCategoria = "";
+
+    static boolean empateminimo = false;
+
+    static boolean empatemaximo = false;
+
+    public enum CodigosCategorias {
+        D, A, M, I, C
+    }
+
     static String convertirCodigoACategoria(String codigo) {
         String categoria = "";
         switch (codigo) {
@@ -33,29 +55,23 @@ public class BarJavier {
         return categoria;
     }
 
-    public static void main(String[] args) {
+    public static void casosdeprueba(String categoria, float valor) {
+        inicializarVentas();
+        contabilizarDia(categoria, valor);
+        maximosMinimosEmpates();
+        imprimirResultado();
+    }
 
-        sc = new java.util.Scanner(System.in);
-        Map<String, Float> ventas = new HashMap<String, Float>();
-        String input = sc.nextLine();
+    public static void inicializarVentas() {
+        for (CodigosCategorias cat : CodigosCategorias.values()) {
+            ventas.put(cat.toString(), 0.0f);
+        }
+    }
+
+    public static void contabilizarDia(String categoria, float valor) {
         String[] entrada = null;
-        while (!input.equals("")) {
-            entrada = input.split(" ");
-            String categoria = entrada[0];
-            float valor = Float.parseFloat(entrada[1]);
-
-            float valorTotal = 0;
-            int numVentas = 0;
-            float valorTotalComidas = 0;
-            int numVentasComidas = 0;
-            ventas.put("D", (float) 0);
-            ventas.put("A", (float) 0);
-            ventas.put("M", (float) 0);
-            ventas.put("I", (float) 0);
-            ventas.put("C", (float) 0);
-
-            while (categoria.equals("D") || categoria.equals("A") || categoria.equals("M") || categoria.equals("I")
-                    || categoria.equals("C")) {
+        while (!(categoria.equals("N") && valor == 0)) {
+            if (ventas.containsKey(categoria)) {
                 ventas.put(categoria, (float) (ventas.get(categoria) + valor));
                 valorTotal = valorTotal + valor;
                 numVentas++;
@@ -63,74 +79,99 @@ public class BarJavier {
                     valorTotalComidas = valorTotalComidas + valor;
                     numVentasComidas++;
                 }
-
-                entrada = sc.nextLine().split(" ");
-                categoria = entrada[0];
-                valor = Float.parseFloat(entrada[1]);
             }
-            // Tenemos el diccionario Ventas relleno, ahora hay que determinar el maximo ,minimo, empate maximo o empate minimo
+            entrada = sc.nextLine().split(" ");
+            categoria = entrada[0];
+            valor = Float.parseFloat(entrada[1]);
+        }
+    }
 
-            if (categoria.equals("N") && valor == 0) {
+    public static void maximosMinimosEmpates() {
 
-                double maximo = 0.0;
-                double minimo = 0.0;
-                String maxCategoria = "";
-                String minCategoria = "";
-                boolean empateminimo = false;
-                boolean empatemaximo = false;
-                Iterator<String> it = ventas.keySet().iterator();
-                for (int i = 0; i < 5; i++) {
-                    String key = (String) it.next();
-                    if (i == 0) {
-                        maximo = ventas.get(key);
-                        minimo = ventas.get(key);
-                        minCategoria = key;
-                        maxCategoria = key;
-                    } else {
-                        if (ventas.get(key) < minimo) {
-                            minimo = ventas.get(key);
-                            minCategoria = key;
-                            empateminimo = false;
-                        } else if (ventas.get(key) > maximo) {
-                            maximo = ventas.get(key);
-                            maxCategoria = key;
-                            empatemaximo = false;
-                        } else if (ventas.get(key) == minimo)
-                            empateminimo = true;
-                        else if (ventas.get(key) == maximo)
-                            empatemaximo = true;
-                    }
-                }
+        double maximo = 0.0;
+        double minimo = 0.0;
 
-                if (minimo == maximo) {
+        Iterator<String> it = ventas.keySet().iterator();
+        for (int i = 0; i < 5; i++) {
+            String key = (String) it.next();
+            if (i == 0) {
+                maximo = ventas.get(key);
+                minimo = ventas.get(key);
+                minCategoria = key;
+                maxCategoria = key;
+            } else {
+                if (ventas.get(key) < minimo) {
+                    minimo = ventas.get(key);
+                    minCategoria = key;
+                    empateminimo = false;
+                } else if (ventas.get(key) > maximo) {
+                    maximo = ventas.get(key);
+                    maxCategoria = key;
+                    empatemaximo = false;
+                } else if (ventas.get(key) == minimo)
                     empateminimo = true;
+                else if (ventas.get(key) == maximo)
                     empatemaximo = true;
-                }
+            }
+        }
 
-                if (empatemaximo) {
-                    System.out.print("EMPATE#");
-                } else {
-                    System.out.print(convertirCodigoACategoria(maxCategoria) + "#");
-                }
-                if (empateminimo) {
-                    System.out.print("EMPATE#");
-                } else {
-                    System.out.print(convertirCodigoACategoria(minCategoria) + "#");
-                }
+        if (minimo == maximo) {
+            empateminimo = true;
+            empatemaximo = true;
+        }
+    }
 
-                String mayorMediaComidas = "NO";
-                float resultadoTotalComidas = 0;
-                if (numVentasComidas > 0)
-                    resultadoTotalComidas = valorTotalComidas / numVentasComidas;
-                float resultadoTotal = valorTotal / numVentas;
-                if (resultadoTotalComidas > resultadoTotal)
-                    mayorMediaComidas = "SI";
+    public static void imprimirResultado() {
+        if (empatemaximo) {
+            System.out.print("EMPATE#");
+        } else {
+            System.out.print(convertirCodigoACategoria(maxCategoria) + "#");
+        }
+        if (empateminimo) {
+            System.out.print("EMPATE#");
+        } else {
+            System.out.print(convertirCodigoACategoria(minCategoria) + "#");
+        }
 
-                System.out.println(mayorMediaComidas);
+        String mayorMediaComidas = "NO";
+        if (mayorMediaComidas()) {
+            mayorMediaComidas = "SI";
+        }
+        System.out.println(mayorMediaComidas);
+
+    }
+
+    public static boolean mayorMediaComidas() {
+        float resultadoTotalComidas = 0;
+        if (numVentasComidas > 0) {
+            resultadoTotalComidas = valorTotalComidas / numVentasComidas;
+        }
+
+        float resultadoTotal = valorTotal / numVentas;
+
+        if (resultadoTotalComidas > resultadoTotal)
+            return true;
+        else
+            return false;
+    }
+
+    public static void main(String[] args) {
+
+        sc = new java.util.Scanner(System.in);
+
+        String input = sc.nextLine();
+        String[] entrada = null;
+        while (!input.equals("")) {
+            try {
+                entrada = input.split(" ");
+                String categoria = entrada[0];
+                float valor = Float.parseFloat(entrada[1]);
+                casosdeprueba(categoria, valor);
+            } catch (ArrayIndexOutOfBoundsException | NumberFormatException ex) {
             }
             input = sc.nextLine();
-
         }
+
         sc.close();
         System.exit(0);
     }
