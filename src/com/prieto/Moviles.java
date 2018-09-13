78f5757dfa76a715302a53d12d43ad6c75b17f12
Luis <prietomoral@gmail.com>
2018-09-13 23:25:57 +0200

    Moviles

--- src/com/prieto/Moviles.java
+++ src/com/prieto/Moviles.java
@@ -1,48 +1,66 @@
 package com.prieto;
 
 public class Moviles {
-	static java.util.Scanner sc;
+    static java.util.Scanner sc;
 
-	public static boolean esBalenceado(int pi, int di, int pd, int dd) {
-		String[] entrada;
-		boolean izda = true;
-		if (pi == 0) {
-			entrada = sc.nextLine().split(" ");
+    static int pesoIzdo = 0;
 
-			izda = esBalenceado(Integer.parseInt(entrada[0]), Integer.parseInt(entrada[1]),
-					Integer.parseInt(entrada[2]), Integer.parseInt(entrada[3]));
-		}
+    static int pesoDcho = 0;
 
-		boolean dcha = true;
-		if (pd == 0) {
-			entrada = sc.nextLine().split(" ");
-			dcha = esBalenceado(Integer.parseInt(entrada[0]), Integer.parseInt(entrada[1]),
-					Integer.parseInt(entrada[2]), Integer.parseInt(entrada[3]));
-		}
+    public static boolean esBalenceado(int pi, int di, int pd, int dd, int p) {
+        String[] entrada;
+        boolean izda = true;
+        if (pi == 0) {
+            entrada = sc.nextLine().split(" ");
+            int ipi = Integer.parseInt(entrada[0]);
+            int idi = Integer.parseInt(entrada[1]);
+            int ipd = Integer.parseInt(entrada[2]);
+            int idd = Integer.parseInt(entrada[3]);
+            izda = esBalenceado(ipi, idi, ipd, idd, pi);
+            pesoIzdo = ipi + ipd;
+        }
 
-		return izda && dcha && pi * di == pd * dd;
-	}
+        boolean dcha = true;
+        if (pd == 0) {
+            entrada = sc.nextLine().split(" ");
+            int dpi = Integer.parseInt(entrada[0]);
+            int ddi = Integer.parseInt(entrada[1]);
+            int dpd = Integer.parseInt(entrada[2]);
+            int ddd = Integer.parseInt(entrada[3]);
+            dcha = esBalenceado(dpi, ddi, dpd, ddd, pd);
+            pesoDcho = dpi + dpd;
+        }
 
-	public static void main(String[] args) {
+        p = pi + pd;
+        if (pi == 0) {
+            pi = pesoIzdo;
+        }
+        if (pd == 0) {
+            pd = pesoDcho;
+        }
+        return izda && dcha && pi * di == pd * dd;
+    }
 
-		sc = new java.util.Scanner(System.in);
-		String[] entrada = sc.nextLine().split(" ");
-		boolean balancea;
-		while (!(entrada[0].equals("0") && entrada[1].equals("0") && entrada[2].equals("0")
-				&& entrada[3].equals("0"))) {
-			balancea = esBalenceado(Integer.parseInt(entrada[0]), Integer.parseInt(entrada[1]),
-					Integer.parseInt(entrada[2]), Integer.parseInt(entrada[3]));
+    public static void main(String[] args) {
 
-			if (balancea) {
-				System.out.println("SI");
-			} else {
-				System.out.println("NO");
-			}
+        sc = new java.util.Scanner(System.in);
+        int peso = 0;
+        String[] entrada = sc.nextLine().split(" ");
+        boolean balancea;
+        while (!(entrada[0].equals("0") && entrada[1].equals("0") && entrada[2].equals("0") && entrada[3].equals("0"))) {
+            balancea = esBalenceado(Integer.parseInt(entrada[0]), Integer.parseInt(entrada[1]), Integer.parseInt(entrada[2]),
+                    Integer.parseInt(entrada[3]), peso);
 
-			entrada = sc.nextLine().split(" ");
-		}
-		sc.close();
-		System.exit(0);
-	}
+            if (balancea) {
+                System.out.println("SI");
+            } else {
+                System.out.println("NO");
+            }
+
+            entrada = sc.nextLine().split(" ");
+        }
+        sc.close();
+        System.exit(0);
+    }
 
 }
