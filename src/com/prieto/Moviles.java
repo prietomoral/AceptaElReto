75a32fd850105dca7e276b63cf35cd8601996776
Luis <prietomoral@gmail.com>
2018-09-14 19:12:38 +0200

    moviles

--- src/com/prieto/Moviles.java
+++ src/com/prieto/Moviles.java
@@ -1,72 +1,88 @@
 package com.prieto;
 
 public class Moviles {
-	static java.util.Scanner sc;
+    static java.util.Scanner sc;
 
-	static int pesoIzdo = 0;
+    static int pesoIzdo = 0;
 
-	static int pesoDcho = 0;
+    static int pesoDcho = 0;
 
-	public static boolean esBalenceado(int pi, int di, int pd, int dd, int[] p) {
-		String[] entrada;
-		boolean izda = true;
-		if (pi == 0) {
-			entrada = sc.nextLine().split(" ");
-			int ipi = Integer.parseInt(entrada[0]);
-			int idi = Integer.parseInt(entrada[1]);
-			int ipd = Integer.parseInt(entrada[2]);
-			int idd = Integer.parseInt(entrada[3]);
-			p[0] = pi;
-			izda = esBalenceado(ipi, idi, ipd, idd, p);
-			if (pi == 0)
-				pesoIzdo = ipi + ipd;
-		}
+    public static boolean esBalenceado(int[] movil) {
+        String[] entrada;
+        int pi = movil[0];
+        int di = movil[1];
+        int pd = movil[2];
+        int dd = movil[3];
+        boolean izda = true;
+        if (pi == 0) {
+            entrada = sc.nextLine().split(" ");
+            int ipi = Integer.parseInt(entrada[0]);
+            int idi = Integer.parseInt(entrada[1]);
+            int ipd = Integer.parseInt(entrada[2]);
+            int idd = Integer.parseInt(entrada[3]);
+            movil[0] = ipi;
+            movil[1] = idi;
+            movil[2] = ipd;
+            movil[3] = idd;
 
-		boolean dcha = true;
-		if (pd == 0) {
-			entrada = sc.nextLine().split(" ");
-			int dpi = Integer.parseInt(entrada[0]);
-			int ddi = Integer.parseInt(entrada[1]);
-			int dpd = Integer.parseInt(entrada[2]);
-			int ddd = Integer.parseInt(entrada[3]);
-			p[1] = pd;
+            izda = esBalenceado(movil);
+            if (pi == 0 && pd == 0)
+                movil[4] = ipi + ipd;
+           
+        }
 
-			dcha = esBalenceado(dpi, ddi, dpd, ddd, p);
-			if (pd == 0)
-				pesoDcho = dpi + dpd;
-		}
+        boolean dcha = true;
+        if (pd == 0) {
+            entrada = sc.nextLine().split(" ");
+            int dpi = Integer.parseInt(entrada[0]);
+            int ddi = Integer.parseInt(entrada[1]);
+            int dpd = Integer.parseInt(entrada[2]);
+            int ddd = Integer.parseInt(entrada[3]);
+            movil[0] = dpi;
+            movil[1] = ddi;
+            movil[2] = dpd;
+            movil[3] = ddd;
 
-		// p[0] = pi + pd;
-		if (pi == 0) {
-			pi = pesoIzdo;
-		}
-		if (pd == 0) {
-			pd = pesoDcho;
-		}
-		return izda && dcha && pi * di == pd * dd;
-	}
+            dcha = esBalenceado(movil);
+            if (pi == 0 && pd == 0)
+                movil[5] = dpi + dpd;
+        }
 
-	public static void main(String[] args) {
+        if (pi == 0) {
+            pi = movil[4];
+            movil[0]=pi;
+        }
+        if (pd == 0) {
+            pd = movil[5];
+            movil[2]=pd;
+        }
 
-		sc = new java.util.Scanner(System.in);
-		int[] peso = new int[2];
-		String[] entrada = sc.nextLine().split(" ");
-		boolean balancea;
-		while (!(entrada[0].equals("0") && entrada[1].equals("0") && entrada[2].equals("0")
-				&& entrada[3].equals("0"))) {
-			balancea = esBalenceado(Integer.parseInt(entrada[0]), Integer.parseInt(entrada[1]),
-					Integer.parseInt(entrada[2]), Integer.parseInt(entrada[3]), peso);
+        return izda && dcha && pi * di == pd * dd;
+    }
 
-			if (balancea) {
-				System.out.println("SI");
-			} else {
-				System.out.println("NO");
-			}
+    public static void main(String[] args) {
 
-			entrada = sc.nextLine().split(" ");
-		}
-		sc.close();
-		System.exit(0);
-	}
+        sc = new java.util.Scanner(System.in);
+        int[] movil = new int[6];
+        String[] entrada = sc.nextLine().split(" ");
+        boolean balancea;
+        while (!(entrada[0].equals("0") && entrada[1].equals("0") && entrada[2].equals("0") && entrada[3].equals("0"))) {
+            for (int i = 0; i < entrada.length; i++) {
+                movil[i] = Integer.parseInt(entrada[i]);
+            }
+
+            balancea = esBalenceado(movil);
+
+            if (balancea) {
+                System.out.println("SI");
+            } else {
+                System.out.println("NO");
+            }
+
+            entrada = sc.nextLine().split(" ");
+        }
+        sc.close();
+        System.exit(0);
+    }
 
 }
