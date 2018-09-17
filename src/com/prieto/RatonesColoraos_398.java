6c6b0400f4af8114d3d5d0bcf0b8ccbcea5bb491
Luis <prietomoral@gmail.com>
2018-09-17 18:43:21 +0200

    ratones

--- src/com/prieto/RatonesColoraos_398.java
+++ src/com/prieto/RatonesColoraos_398.java
@@ -1,42 +1,69 @@
 package com.prieto;
 
 public class RatonesColoraos_398 {
-	static java.util.Scanner sc;
-	
-	
-	public static void casosdeprueba() {
-		int n = sc.nextInt();
-	   //Mientras n > 0 HACER
-//		LeerObjetivos()
-//		SI segR1 <= segR2 ENTONCES
-//			SI segundosPosOBj <= segundosTotal ENTONCES
-//			  segundoTotal = segundoTOTAL + 0
-//			SINO
-//			  segundoTotal = segundoTOTAL + segR1
-//		    FINSI
-//		SINO SI segR1 > segR2 ENTONCES
-//		
-//		
-//		
-//		n--
-//		FIN MIENTRAS
-//		
-//		Calcular Maximo hasta el final
-//		sumar 1
-//		
-	}
-	
-	
-	
-	public static void main(String[] args) {
-		
-		sc = new java.util.Scanner(System.in);
-		int numcasos = sc.nextInt();
-		for (int i = 0; i < numcasos; i++) {
-			casosdeprueba();
-		}
-		sc.close();
-		System.exit(0);
-	}
+    static java.util.Scanner sc;
+
+    public static int obtenerTiempo(int[] pos1, int[] pos2) {
+        return Math.abs(pos1[0] - pos2[0]) + Math.abs(pos1[1] - pos2[1]);
+    }
+
+    public static void casosdeprueba(String entrada) {
+        String[] posiciones = new String[2];
+        posiciones = entrada.split(" ");
+        int F = Integer.parseInt(posiciones[0]);
+        int C = Integer.parseInt(posiciones[1]);
+        int SALIDA = 1;
+        int N = Integer.parseInt(sc.nextLine());
+        int[] posObjetivo = new int[2];
+        int[] posInicial = {1, 1};
+        int[] posFinal = {F, C};
+        int[] posRaton1 = {1, 1};
+        int[] posRaton2 = {1, 1};
+        int segRaton1;
+        int segRaton2;
+        int segTotales = 0;
+        int segPosInicialObjetivo;
+        while (N > 0) {
+            posiciones = sc.nextLine().split(" ");
+            posObjetivo[0] = Integer.parseInt(posiciones[0]);
+            posObjetivo[1] = Integer.parseInt(posiciones[1]);
+            segRaton1 = obtenerTiempo(posRaton1, posObjetivo);
+            segRaton2 = obtenerTiempo(posRaton2, posObjetivo);
+            segPosInicialObjetivo = obtenerTiempo(posInicial, posObjetivo);
+            if (segRaton1 <= segRaton2) {
+                if (segPosInicialObjetivo > segTotales) {
+                    segTotales = segTotales + segRaton1;
+                }
+                posRaton1[0] = posObjetivo[0];
+                posRaton1[1] = posObjetivo[1];
+            } else {
+                if (segPosInicialObjetivo > segTotales) {
+                    segTotales = segTotales + segRaton2;
+                }
+                posRaton2[0] = posObjetivo[0];
+                posRaton2[1] = posObjetivo[1];
+
+            }
+            N--;
+        }
+
+        // Falta calcular el tiempo maximo que les queda hasta el final
+        int tiempoFinal = Math.max(obtenerTiempo(posRaton1, posFinal), obtenerTiempo(posRaton2, posFinal));
+        segTotales = segTotales + tiempoFinal + SALIDA;
+        System.out.println(segTotales);
+
+    }
+
+    public static void main(String[] args) {
+
+        sc = new java.util.Scanner(System.in);
+        String entrada = sc.nextLine();
+        while (!entrada.equals("")) {
+            casosdeprueba(entrada);
+            entrada = sc.nextLine();
+        }
+        sc.close();
+        System.exit(0);
+    }
 
 }
