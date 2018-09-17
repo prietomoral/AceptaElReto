d7b5385600cda0c845701c6f0f3937bac9ea0e1a
luisjavier.prieto <luisjavier.prieto@juntaex.es>
2018-09-17 14:39:53 +0200

    ratones coloraos

---
+++ src/com/prieto/RatonesColoraos_398.java
@@ -0,0 +1,42 @@
+package com.prieto;
+
+public class RatonesColoraos_398 {
+	static java.util.Scanner sc;
+	
+	
+	public static void casosdeprueba() {
+		int n = sc.nextInt();
+	   //Mientras n > 0 HACER
+//		LeerObjetivos()
+//		SI segR1 <= segR2 ENTONCES
+//			SI segundosPosOBj <= segundosTotal ENTONCES
+//			  segundoTotal = segundoTOTAL + 0
+//			SINO
+//			  segundoTotal = segundoTOTAL + segR1
+//		    FINSI
+//		SINO SI segR1 > segR2 ENTONCES
+//		
+//		
+//		
+//		n--
+//		FIN MIENTRAS
+//		
+//		Calcular Maximo hasta el final
+//		sumar 1
+//		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+		sc = new java.util.Scanner(System.in);
+		int numcasos = sc.nextInt();
+		for (int i = 0; i < numcasos; i++) {
+			casosdeprueba();
+		}
+		sc.close();
+		System.exit(0);
+	}
+
+}
