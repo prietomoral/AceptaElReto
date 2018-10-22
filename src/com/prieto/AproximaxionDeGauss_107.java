a6fc50f4edc2c2e972473be9c0314c3d5c24098d
luisjavier.prieto <luisjavier.prieto@juntaex.es>
2018-10-22 12:29:42 +0200

    optimizando gauss

--- src/com/prieto/AproximaxionDeGauss_107.java
+++ src/com/prieto/AproximaxionDeGauss_107.java
@@ -1,57 +1,89 @@
 package com.prieto;
 
 public class AproximaxionDeGauss_107 {
-    static java.util.Scanner sc;
+	static java.util.Scanner sc;
+	static double[] erroresPermitidos = { 1, 0.1, 0.01, 0.001, 0.0001, 0.00001 };
 
-    public static void casosdeprueba(int n, int m) {
-        double numPrimos = numeroDePrimos(n);
+	public static void casosdeprueba(int n, int m) {
+		if (m == 1) {
+			System.out.println("Menor");
+		} else {
 
-        double errorGauss = Math.abs((double) (numPrimos / (double) n) - (double) ((double) 1 / (double) Math.log(n)));
+			double numPrimos = numeroDePrimos(n);
 
-        double errorPermitido = (double) 1 / (double) Math.pow(10, m);
+			double errorGauss = Math
+					.abs((double) (numPrimos / (double) n) - (double) ((double) 1 / (double) Math.log(n)));
 
-        if (errorGauss > errorPermitido) {
-            System.out.println("Mayor");
-        } else if (errorGauss < errorPermitido) {
-            System.out.println("Menor");
-        } else {
-            System.out.println("Igual");
-        }
-    }
+			double errorPermitido = erroresPermitidos[m]; // (double) 1 / (double) Math.pow(10, m);
 
-    static int numeroDePrimos(int n) {
-        int i, j;
-        boolean esPrimo;
-        int rInicial = 2;// Rango inicial, este debe ser mayor de 1.
-        int rFinal = n;// Rango final.
-        int cont = 0;
-        for (i = rInicial; i <= rFinal; i++) {
-            // recorro ciclo tantas veces como necesite(<= es para incluir el valor de rFinal).
-            esPrimo = true;
-            for (j = 2; j < i && esPrimo; j++) {
-                // no coloque j = 1 porque ya sabemos que todo numero es divisible por 1.
-                /* j < i es para no incluir el numero a evaluar, pues todo numero es divisible por si mismo. */
-                if (i % j == 0) {// Si además del 1 y el mismo hay otro divisor, ya no es primo.
-                    esPrimo = false;
-                }
-            }
-            if (esPrimo) {// Si es primo lo imprimo.
-                cont++;
-            }
-        }
-        return cont;
-    }
+			if (errorGauss > errorPermitido) {
+				System.out.println("Mayor");
+			} else if (errorGauss < errorPermitido) {
+				System.out.println("Menor");
+			} else {
+				System.out.println("Igual");
+			}
+		}
+	}
 
-    public static void main(String[] args) {
+	static boolean es_primo(int p) {
+		if (p == 1)
+			return false; // 1 no es primo
+		if (p == 2)
+			return true; // 2 es primo
+		if (p % 2 == 0)
+			return false; // no es primo ning�n par, excepto el 2
+		double raiz = Math.sqrt(p);
+		for (long i = 3; i <= raiz; i += 2) {
+			if (p % i == 0)
+				return false;
+		}
+		return true;
+	}
 
-        sc = new java.util.Scanner(System.in);
-        String[] entrada = sc.nextLine().split(" ");
-        while (!(entrada[0].equals("0") && entrada[1].equals("0"))) {
-            casosdeprueba(Integer.parseInt(entrada[0]), Integer.parseInt(entrada[1]));
-            entrada = sc.nextLine().split(" ");
-        }
-        sc.close();
-        System.exit(0);
-    }
+	static int numeroDePrimos(int n) {
+		int i, j;
+		boolean esPrimo;
+		int rInicial = 2;// Rango inicial, este debe ser mayor de 1.
+		int rFinal = n;// Rango final.
+		int cont = 0;
+		for (i = rInicial; i <= rFinal; i++) {
+			// recorro ciclo tantas veces como necesite(<= es para incluir el valor de
+			// rFinal).
+//			esPrimo = true;
+//			for (j = 2; j < i && esPrimo; j++) {
+//				// no coloque j = 1 porque ya sabemos que todo numero es divisible por 1.
+//				/*
+//				 * j < i es para no incluir el numero a evaluar, pues todo numero es divisible
+//				 * por si mismo.
+//				 */
+//				if (i % j == 0) {// Si además del 1 y el mismo hay otro divisor, ya no es primo.
+//					esPrimo = false;
+//				}
+//			}
+
+			esPrimo = es_primo(i);
+			if (esPrimo) {
+				cont++;
+			}
+		}
+		return cont;
+	}
+
+	public static void main(String[] args) {
+
+		sc = new java.util.Scanner(System.in);
+		String[] entrada = sc.nextLine().split(" ");
+		int n = Integer.parseInt(entrada[0]);
+		int m = Integer.parseInt(entrada[1]);
+		while (!(n == 0 && m == 0)) {
+			casosdeprueba(n, m);
+			entrada = sc.nextLine().split(" ");
+			n = Integer.parseInt(entrada[0]);
+			m = Integer.parseInt(entrada[1]);
+		}
+		sc.close();
+		System.exit(0);
+	}
 
 }
