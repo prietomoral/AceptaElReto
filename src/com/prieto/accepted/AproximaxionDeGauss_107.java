cc97b5a20238a00a6807d27a156fee594eb62558
Luis <prietomoral@gmail.com>
2018-10-25 16:45:52 +0200

    Aproximacion de Gauss accepted

--- src/com/prieto/accepted/AproximaxionDeGauss_107.java
+++ src/com/prieto/accepted/AproximaxionDeGauss_107.java
@@ -1,15 +1,18 @@
-package com.prieto;
+package com.prieto.accepted;
+
 
 public class AproximaxionDeGauss_107 {
 	static java.util.Scanner sc;
 	static double[] erroresPermitidos = { 1, 0.1, 0.01, 0.001, 0.0001, 0.00001 };
-
+	static Integer [] primos = new Integer [100005];
+	
 	public static void casosdeprueba(int n, int m) {
 		if (m == 1) {
 			System.out.println("Menor");
 		} else {
-
-			double numPrimos = numeroDePrimos(n);
+		    double numPrimos;
+			// numPrimos = numeroDePrimos(n);
+			numPrimos = primos[n];
 
 			double errorGauss = Math
 					.abs((double) (numPrimos / (double) n) - (double) ((double) 1 / (double) Math.log(n)));
@@ -32,7 +35,7 @@ public class AproximaxionDeGauss_107 {
 		if (p == 2)
 			return true; // 2 es primo
 		if (p % 2 == 0)
-			return false; // no es primo ningún par, excepto el 2
+			return false; // no es primo ningï¿½n par, excepto el 2
 		double raiz = Math.sqrt(p);
 		for (long i = 3; i <= raiz; i += 2) {
 			if (p % i == 0)
@@ -70,9 +73,28 @@ public class AproximaxionDeGauss_107 {
 		return cont;
 	}
 
+	static void inicializarPrimos() {
+        int i;
+        boolean esPrimo;
+        int rInicial = 2;// Rango inicial, este debe ser mayor de 1.
+        primos[0]=0;
+        primos[1]=0;
+        int rFinal = 100001;// Rango final.
+        int cont = 0;
+        for (i = rInicial; i <= rFinal; i++) {
+            esPrimo = es_primo(i);
+            if (esPrimo) {
+                cont++;
+            }
+            primos[i]=cont;
+        }
+    }
+
+	
 	public static void main(String[] args) {
 
 		sc = new java.util.Scanner(System.in);
+        inicializarPrimos();
 		String[] entrada = sc.nextLine().split(" ");
 		int n = Integer.parseInt(entrada[0]);
 		int m = Integer.parseInt(entrada[1]);
