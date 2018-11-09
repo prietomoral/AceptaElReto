fe972587453afb07832dae786f9fc73fa2735bd7
Luis <prietomoral@gmail.com>
2018-11-09 20:52:38 +0100

    Palmeras accepted

--- src/com/prieto/accepted/PalmerasEnLaNieve_320.java
+++ src/com/prieto/accepted/PalmerasEnLaNieve_320.java
@@ -1,9 +1,11 @@
-package com.prieto;
+package com.prieto.accepted;
 
 public class PalmerasEnLaNieve_320 {
     static java.util.Scanner sc;
 
     static boolean[] paseo;
+
+    static int[] franja;
 
     static final int DEBILIDAD = 5;
 
@@ -34,7 +36,7 @@ public class PalmerasEnLaNieve_320 {
         return longitud;
     }
 
-    public static void casosdeprueba() {
+    public static void casosdeprueba2() {
         int pesoTotal = Integer.parseInt(sc.nextLine());
         int palmerasTotales = Integer.parseInt(sc.nextLine());
         inicializarPaseo(pesoTotal, palmerasTotales);
@@ -52,6 +54,33 @@ public class PalmerasEnLaNieve_320 {
             }
         }
         System.out.println(longitudMaxima);
+    }
+
+    public static void casosdeprueba() {
+        int pesoTotal = Integer.parseInt(sc.nextLine());
+        int palmeras = Integer.parseInt(sc.nextLine());
+        franja = new int[palmeras];
+        int i = 0;
+        int j = 0;
+        int l = 0;
+        int enPie = 0;
+
+        while (palmeras-- >0) {
+            franja[j] = sc.nextInt();
+            if (franja[j] >= pesoTotal && ++enPie > DEBILIDAD) {
+                while (enPie > DEBILIDAD) {
+                    if (franja[i] >= pesoTotal) {
+                        enPie--;
+                    }
+                    i++;
+                }
+
+            }
+            l = Math.max(l, j - i + 1);
+            j++;
+        }
+        System.out.println(l);
+        sc.nextLine();
     }
 
     public static void main(String[] args) {
